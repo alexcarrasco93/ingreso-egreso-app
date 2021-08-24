b9e1a85d400cddf0e7d23af935254ac41a06a598
Alex <alexcarrascosalvador@gmail.com>
2021-08-24 13:06:16 +0200

    logut - authListener

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -7,10 +7,23 @@ import { AngularFireAuth } from '@angular/fire/auth';
 export class AuthService {
   constructor(private auth: AngularFireAuth) {}
 
+  initAuthListener() {
+    this.auth.authState.subscribe((fuser) => {
+      console.log(fuser);
+      console.log(fuser?.uid);
+      console.log(fuser?.email);
+    });
+  }
+
   crearUsuario(nombre: string, email: string, password: string) {
     return this.auth.createUserWithEmailAndPassword(email, password);
   }
+
   loginUsuario(email: string, password: string) {
     return this.auth.signInWithEmailAndPassword(email, password);
+  }
+
+  logout() {
+    return this.auth.signOut();
   }
 }
--- src/app/shared/sidebar/sidebar.component.ts
+++ src/app/shared/sidebar/sidebar.component.ts
@@ -1,16 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
-  styles: [
-  ]
+  styles: [],
 })
 export class SidebarComponent implements OnInit {
+  constructor(private authService: AuthService, private router: Router) {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit(): void {
+  logout() {
+    this.authService.logout().then(() => this.router.navigate(['/login']));
   }
-
 }
